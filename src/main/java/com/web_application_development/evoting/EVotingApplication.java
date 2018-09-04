ea1dbf6a4490fb1f8e7399c168e1900989b1c870
kertmannikut <kert.mannik@tudeng.ut.ee>
2018-09-04 15:15:00 +0300

    Gradle project setup using Spring Initializr.

---
+++ src/main/java/com/web_application_development/evoting/EVotingApplication.java
@@ -0,0 +1,12 @@
+package com.web_application_development.evoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EVotingApplication.class, args);
+	}
+}
