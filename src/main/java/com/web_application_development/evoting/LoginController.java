e9a7625318d17eb199de74da08ea59c467a71d3a
kristiinakeps <kristiina797@gmail.com>
2018-09-17 16:19:35 +0300

    created controllers

---
+++ src/main/java/com/web_application_development/evoting/LoginController.java
@@ -0,0 +1,13 @@
+package com.web_application_development.evoting;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String getTestPage() {
+        return "login/index";
+    }
+}
