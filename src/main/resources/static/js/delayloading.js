88c527722c3dd6bc3a3856f089a9b27a525312ac
kertmannik <kert.mannik@gmail.com>
2018-10-03 18:58:01 +0300

    Class change after timeout: loading img smaller and real img bigger

--- src/main/resources/static/js/delayloading.js
+++ src/main/resources/static/js/delayloading.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
       var imagex = $(this);
       var imgOriginal = imagex.data('original');
       $(imagex).attr('src', imgOriginal);
+      $(imagex).attr("class","image-after");
     });
   }, 3000);
 });
