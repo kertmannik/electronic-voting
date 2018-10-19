36cc908924610034c6d83e1dc2e24a7446887733
kertmannik <kert.mannik@gmail.com>
2018-10-19 20:34:08 +0300

    Remove redundant code

--- src/main/java/com/web_application_development/evoting/controllers/VoteController.java
+++ src/main/java/com/web_application_development/evoting/controllers/VoteController.java
@@ -1,12 +1,10 @@
 package com.web_application_development.evoting.controllers;
 
-import com.google.gson.Gson;
 import com.web_application_development.evoting.dtos.VoteDTO;
 import com.web_application_development.evoting.entities.Candidate;
 import com.web_application_development.evoting.services.MasterService;
 import ee.sk.smartid.AuthenticationIdentity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,20 +27,12 @@ public class VoteController {
     }
 
     @PostMapping(value = "/vote", produces = APPLICATION_JSON)
-    public ResponseEntity<Void> sendVote(@RequestBody String candidateId) {
-        VoteDTO voteDTO = fromJsonToJava(candidateId);
+    public void sendVote(@RequestBody VoteDTO voteDTO) {
         AuthenticationIdentity authIdentity = ((AuthenticationIdentity) (SecurityContextHolder.getContext().getAuthentication()).getPrincipal());
         // save new entity
         masterService.saveVote(voteDTO, authIdentity.getIdentityCode());
 
         Candidate candidate = masterService.findCandidateById(voteDTO.getCandidateId());
         messagingTemplate.convertAndSend("/topic/votes", candidate);
-
-        return ResponseEntity.ok().build();
-    }
-
-    private VoteDTO fromJsonToJava(String rawData) {
-        final Gson gson = new Gson();
-        return gson.fromJson(rawData, VoteDTO.class);
     }
 }
--- src/main/java/com/web_application_development/evoting/dtos/VoteDTO.java
+++ src/main/java/com/web_application_development/evoting/dtos/VoteDTO.java
@@ -1,13 +1,18 @@
 package com.web_application_development.evoting.dtos;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
+@NoArgsConstructor
 @Getter
 @Setter
 public class VoteDTO {
+
     private Integer candidateId;
+
+    public VoteDTO() {
+    }
 
     public VoteDTO(Integer candidateId) {
         this.candidateId = candidateId;
