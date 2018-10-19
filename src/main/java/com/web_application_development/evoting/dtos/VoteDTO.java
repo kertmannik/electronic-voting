e31d6314d47de824c814b6705acc126c5e32c047
kertmannik <kert.mannik@gmail.com>
2018-10-19 20:38:45 +0300

    Fix duplicate constructors

--- src/main/java/com/web_application_development/evoting/dtos/VoteDTO.java
+++ src/main/java/com/web_application_development/evoting/dtos/VoteDTO.java
@@ -1,10 +1,8 @@
 package com.web_application_development.evoting.dtos;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
 @Getter
 @Setter
 public class VoteDTO {
