7a3b54c174d33f2cfa4e2ccf0da122ba282c7865
kertmannik <kert.mannik@gmail.com>
2018-10-23 13:09:05 +0300

    Add new lines

--- src/main/java/com/web_application_development/evoting/smartid/SmartIdController.java
+++ src/main/java/com/web_application_development/evoting/smartid/SmartIdController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
