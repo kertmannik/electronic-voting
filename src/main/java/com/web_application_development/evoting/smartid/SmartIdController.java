0459ee0a9ca3e27d7a191ea8f0691246d982236d
kertmannik <kert.mannik@gmail.com>
2018-10-23 15:24:26 +0300

    Refactor smart id welcome message

--- src/main/java/com/web_application_development/evoting/smartid/SmartIdController.java
+++ src/main/java/com/web_application_development/evoting/smartid/SmartIdController.java
@@ -7,21 +7,28 @@ import ee.sk.smartid.exception.UserAccountNotFoundException;
 import ee.sk.smartid.exception.UserRefusedException;
 import ee.sk.smartid.rest.dao.NationalIdentity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 @RestController
 @RequestMapping(value = "/smart-id/authentication")
 public class SmartIdController {
+
     private final SmartIdClient smartIdClient;
+    private final MessageSource messageSource;
+
     @Autowired
-    SmartIdController(SmartIdClient smartIdClient) {
+    SmartIdController(SmartIdClient smartIdClient, MessageSource messageSource) {
         this.smartIdClient = smartIdClient;
+        this.messageSource = messageSource;
     }
     @PostMapping(value = "/start")
     public Verification startAuthentication(@RequestBody NationalIdentity nationalIdentity, HttpSession httpSession) {
@@ -40,7 +47,7 @@ public class SmartIdController {
                 .withNationalIdentity((NationalIdentity) httpSession.getAttribute("nationalIdentity"))
                 .withAuthenticationHash((AuthenticationHash) httpSession.getAttribute("authenticationHash"))
                 .withCertificateLevel("QUALIFIED") // Certificate level can either be "QUALIFIED" or "ADVANCED"
-                .withDisplayText("Sisselogimine")
+                .withDisplayText(messageSource.getMessage("smartid.welcome", Collections.emptyList().toArray(), LocaleContextHolder.getLocale()))
                 .authenticate();
         AuthenticationResponseValidator authenticationResponseValidator = new AuthenticationResponseValidator();
         SmartIdAuthenticationResult authenticationResult = authenticationResponseValidator.validate(authenticationResponse);
