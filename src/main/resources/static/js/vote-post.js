4aa18aac9a5b14173230a0696a5c0ab1562c8877
kertmannik <kert.mannik@gmail.com>
2018-10-25 18:28:51 +0300

    Add error message to voting

--- src/main/resources/static/js/vote-post.js
+++ src/main/resources/static/js/vote-post.js
@@ -1,5 +1,7 @@
 $(function() {
+    $("#error-text")[0].style.display = 'none';
     $(".btn-block").click(function(e) {
+        $("#error-text")[0].style.display = 'none';
         e.preventDefault();
         console.log($(this).val())
         $.ajax({
@@ -13,6 +15,7 @@ $(function() {
             error: function (data) {
                 console.log("Failure");
                 console.log(data);
+                $("#error-text")[0].style.display = 'block';
             },
             data: JSON.stringify(getFormData($(this).val()))
         });
