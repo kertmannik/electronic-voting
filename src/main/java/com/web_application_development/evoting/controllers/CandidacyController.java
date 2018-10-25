417d09a4ef1d6b537875ea88603a659d56d5a586
kertmannik <kert.mannik@gmail.com>
2018-10-25 16:10:16 +0300

    Add form feedback

--- src/main/java/com/web_application_development/evoting/controllers/CandidacyController.java
+++ src/main/java/com/web_application_development/evoting/controllers/CandidacyController.java
@@ -5,13 +5,17 @@ import com.web_application_development.evoting.services.CandidateService;
 import com.web_application_development.evoting.services.UserStatisticsService;
 import ee.sk.smartid.AuthenticationIdentity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
 
 @Controller
 public class CandidacyController {
@@ -19,12 +23,14 @@ public class CandidacyController {
     private final HttpServletRequest request;
     private final CandidateService candidateService;
     private final UserStatisticsService userStatisticsService;
+    private final MessageSource messageSource;
 
     @Autowired
-    public CandidacyController(HttpServletRequest request, CandidateService candidateService, UserStatisticsService userStatisticsService) {
+    public CandidacyController(HttpServletRequest request, CandidateService candidateService, UserStatisticsService userStatisticsService, MessageSource messageSource) {
         this.request = request;
         this.candidateService = candidateService;
         this.userStatisticsService = userStatisticsService;
+        this.messageSource = messageSource;
     }
 
     @GetMapping(path = "/candidacy")
@@ -34,14 +40,16 @@ public class CandidacyController {
     }
 
     @PostMapping(path = "/candidacy")
-    public String sendSubscription(@ModelAttribute CandidateDTO candidateDTO) {
-        userStatisticsService.saveUserStatistics(request, "/candidacy");
-        AuthenticationIdentity authIdentity = ((AuthenticationIdentity) (SecurityContextHolder.getContext().getAuthentication()).getPrincipal());
-
-        candidateService.saveCandidate(candidateDTO, authIdentity);
-
-        // redirect to home page where all candidates are displayed
-        return "redirect:/";
+    public String sendSubscription(@ModelAttribute CandidateDTO candidateDTO, Model model) {
+        try {
+            userStatisticsService.saveUserStatistics(request, "/candidacy");
+            AuthenticationIdentity authIdentity = ((AuthenticationIdentity) (SecurityContextHolder.getContext().getAuthentication()).getPrincipal());
+            candidateService.saveCandidate(candidateDTO, authIdentity);
+            model.addAttribute("candidacySuccessMessage", messageSource.getMessage("candidacy.successmessage", Collections.emptyList().toArray(), LocaleContextHolder.getLocale()));
+        } catch (Exception exception) {
+            model.addAttribute("candidacyErrorMessage", messageSource.getMessage("candidacy.successmessage", Collections.emptyList().toArray(), LocaleContextHolder.getLocale()));
+        }
+        return "candidacy/index";
     }
 
 }
