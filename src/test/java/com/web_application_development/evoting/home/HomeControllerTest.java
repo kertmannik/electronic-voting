aee62e5f877f16ae806ac1602786c85e4990206a
ckitama <marionlaur94@gmail.com>
2018-11-15 19:57:29 +0200

    Test that homepage candidate table gets populated

---
+++ src/test/java/com/web_application_development/evoting/home/HomeControllerTest.java
@@ -0,0 +1,90 @@
+package com.web_application_development.evoting.home;
+
+import com.web_application_development.evoting.configurations.SecurityConfiguration;
+import com.web_application_development.evoting.controllers.HomeController;
+import com.web_application_development.evoting.dtos.CandidateForVotingDTO;
+import com.web_application_development.evoting.entities.Candidate;
+import com.web_application_development.evoting.repositories.CandidateRepository;
+import com.web_application_development.evoting.services.CandidateService;
+import com.web_application_development.evoting.services.UserStatisticsService;
+import com.web_application_development.evoting.services.VoteService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.Model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+@Import(SecurityConfiguration.class)
+public class HomeControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private SimpMessageSendingOperations messagingTemplate;
+    @MockBean
+    private CandidateRepository candidateRepository;
+    @MockBean
+    private CandidateService candidateService;
+    @MockBean
+    private VoteService voteService;
+    @MockBean
+    private UserStatisticsService userStatisticsService;
+    @Captor
+    private ArgumentCaptor<Candidate> subscriptionArgumentCaptor;
+    @Captor
+    private ArgumentCaptor<CandidateForVotingDTO> subscriptionDTOArgumentCaptor;
+
+
+    @Mock
+    private Model model;
+
+    @Test
+    public void testModelIsPopulatedWithDataFromDB() throws Exception {
+        List<Candidate> candidateList = new ArrayList<>();
+        Candidate candidate = new Candidate();
+        candidate.setFirstName("Mari");
+        candidate.setLastName("Mets");
+        candidate.setIdentityCode("12345678910");
+        candidate.setId(1L);
+        candidate.setRegion("Makumaa");
+        candidate.setParty("Makud");
+        candidate.setHasWithdrawn(0L);
+        candidate.setCandidacyAnnounced(LocalDateTime.now());
+        candidateList.add(candidate);
+
+        when(candidateService.findAllCandidates()).thenReturn(candidateList);
+
+        this.mockMvc.perform(get("/"))
+                // just for debugging purpose
+                // .andDo(print())
+                .andExpect(content().string(containsString("Mari")))
+                .andExpect(content().string(containsString("Mets")))
+                .andExpect(content().string(containsString("1")))
+                .andExpect(content().string(containsString("Makumaa")))
+                .andExpect(content().string(containsString("Makud")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home/index"))
+                .andReturn();
+    }
+}
