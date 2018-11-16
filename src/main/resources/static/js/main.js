14939e7913321db4e15f55634cba36e19175f878
kristiinakeps <kristiina797@gmail.com>
2018-11-16 10:47:46 +0200

    fix table filters

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -8,71 +8,6 @@ $(window).on('load', function () {
     $('#banner').modal('show');
 });
 
-//statistics chart
-// Load google charts
-google.charts.load('current', {'packages': ['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-
-// Draw the chart and set the chart values
-function drawChart() {
-    var myTableArray = [];
-    $("table#votesForPartyTable tr").each(function () {
-        var arrayOfThisRow = [];
-        var tableData = $(this).find('td');
-        if (tableData.length > 0) {
-            tableData.each(function () {
-                arrayOfThisRow.push($(this).text());
-            });
-            myTableArray.push(arrayOfThisRow);
-        }
-    });
-
-    var arrayLength = myTableArray.length;
-    var arrayForChart = [];
-    arrayForChart.push(['Party', 'Votes']);
-    for (var i = 0; i < arrayLength; i++) {
-        var row = [];
-        for (var j = 0; j < myTableArray[i].length; j++) {
-            if (j === 1) {
-                row.push(Number(myTableArray[i][j]));
-            } else {
-                row.push(myTableArray[i][j]);
-            }
-        }
-        arrayForChart.push(row);
-    }
-
-    var chartwidth = $('#chartparent').width();
-    var data = google.visualization.arrayToDataTable(arrayForChart);
-    var options = {'backgroundColor': 'transparent'};
-    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-    chart.draw(data, options);
-}
-
-$(window).resize(function () {
-    drawChart();
-});
-
-//homepage table filter
-$(document).ready(function () {
-    var selectedOption = document.getElementById("selected-language").innerHTML;
-    if (selectedOption.trim() === "et") {
-        $('#candidateTable').DataTable({
-            "info": false,
-            "columnDefs": [
-                {"orderable": false, "targets": 5}
-            ],
-            "language": {"url": "https://cdn.datatables.net/plug-ins/1.10.19/i18n/Estonian.json"}
-        });
-    } else {
-        $('#candidateTable').DataTable({
-            "info": false,
-            "columnDefs": [
-                {"orderable": false, "targets": 5}
-            ]
-        });
-    }
-});
 //map
 //Sain inspriratsiooni eelmise aasta projektist, aga tegin palju ümber
 function initMap() {
@@ -94,21 +29,6 @@ function initMap() {
         map.setCenter(marker.getPosition());
     });
 }
-
-//statistics table filter
-$(document).ready(function () {
-    var selectedOption = document.getElementById("selected-language").innerHTML;
-    if (selectedOption.trim() === "et") {
-        $('#votesTable').DataTable({
-            "info": false,
-            "language": {"url": "https://cdn.datatables.net/plug-ins/1.10.19/i18n/Estonian.json"}
-        });
-    } else {
-        $('#votesTable').DataTable({
-            "info": false
-        });
-    }
-});
 
 //voting
 /**
